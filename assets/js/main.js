29350434a0a87d72c7b4ed934800593dc15da9b6
UniversoCaotico1997 <loria.roberto97@gmail.com>
2022-10-11 15:35:30 +0200

    chiedere la mail all'utente

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,35 @@
 // Mail
 // Chiedi all’utente la sua email, controlla che sia nella lista di chi può accedere, stampa un messaggio appropriato sull’esito del controllo.
+
+
+
+// Strumenti 
+// Prompt 
+// Array
+// Array + ciclo 
+// insertAdjacentHTML 
+
+// Operazioni da eseguire:
+
+// Utiliziamo il comando prompt per chiedere la mail all'utente
+const emailToInsert = prompt(`Inserisci la tua mail!`)
+console.log(emailToInsert);
+
+// Successivamente creiamo una lista con le varie mail 
+
+
+// Attravesro Array + ciclo visioniamo se la mail è presente nella lista 
+//Infine stampiamo sul foglio HTML il risultato 
+
+
 // Gioco dei dadi
 // Generare un numero random da 1 a 6, sia per il giocatore sia per il computer. Stabilire il vincitore, in base a chi fa il punteggio più alto.
 // Prima di partire a scrivere codice poniamoci qualche domanda: Che ci sia un array da qualche parte? Se dobbiamo confrontare qualcosa che "cosa" ci serve?
+
+
+
+
+
 // Consigli del giorno:
 // scriviamo sempre prima dei commenti in italiano per capire cosa vogliamo fare
 // javascript non fa nulla da solo, dobbiamo dirgli noi cosa vogliamo fare
